2eac18a22a15c7cacac0085933fbab2b6910b935
kgonapa <[kasiviswanadh1008@gmail.com]>
2024-03-23 13:35:13 +0100

    file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Kasiviswanadh',
+    author_email='kasiviswanadh1008@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+    
+)
